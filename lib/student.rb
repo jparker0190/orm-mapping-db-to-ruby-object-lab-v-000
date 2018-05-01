87d0a22cddf9eda2a9d85794b4f06aca6dfba815
Joseph Parker <jparker010190@gmail.com>
2018-05-01 22:53:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,7 +76,7 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end
-  def self.all_students_in_grade_x(grade)
+  def self.all_students_in_grade_x
     sql <<-SQL
     SELECT * FROM students where grade = #{grade}
     SQL
